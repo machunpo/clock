08a45f03f0e734de0bdbae3283ff3fa347d0efa4
machunpo <machunpo@aliyun.com>
2020-08-03 15:07:36 +0800

    test

--- clock/test/test.ino
+++ clock/test/test.ino
@@ -8,9 +8,9 @@ int8_t TimeDisp[] = {0x08, 0x08, 0x08, 0x08};
 int8_t Year = 20;
 int8_t Moon = 8;
 int8_t Day = 2;
-//int8_t Hour = 20;
-//int8_t Min = 0;
-//int8_t Sec = 15;
+int8_t Hour = 20;
+int8_t Min = 30;
+int8_t Sec = 0;
 
 void setup()
 {
@@ -27,26 +27,28 @@ void setup()
 void loop()
 {
 
-    for (int8_t Hour=0; Hour < 24; Hour++)
+  for (; Hour < 24; Hour++)
+  {
+    for (; Min < 60; Min++)
     {
-      for (int8_t Min=0; Min < 60; Min++)
+      for (; Sec < 60; Sec++)
       {
-        for (int8_t Sec=0; Sec < 60; Sec++)
-        {
-          TimeDisp[0] = Hour / 10;
-          TimeDisp[1] = Hour % 10;
-          TimeDisp[2] = Min / 10;
-          TimeDisp[3] = Min % 10;
-          tm1637.point(POINT_ON);
-          tm1637.display(TimeDisp);
-          delay(500);
-          tm1637.point(POINT_OFF);
-          tm1637.display(TimeDisp);
-          delay(500);
-        }
+        TimeDisp[0] = Hour / 10;
+        TimeDisp[1] = Hour % 10;
+        TimeDisp[2] = Min / 10;
+        TimeDisp[3] = Min % 10;
+        tm1637.point(POINT_ON);
+        tm1637.display(TimeDisp);
+        delay(500);
+        tm1637.point(POINT_OFF);
+        tm1637.display(TimeDisp);
+        delay(500);
       }
+      Sec = 0;
     }
-    //Sec=1; //调整误差
+    Min = 0;
+  }
+  Hour = 0;
 }
-        // Whether to light the clock point ":".是否点亮时钟点“：
-        // To take effect the next time it displays.下次显示时生效
+// Whether to light the clock point ":".是否点亮时钟点“：
+// To take effect the next time it displays.下次显示时生效
