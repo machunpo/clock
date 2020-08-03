66aeeb35a8966be290281b87dfab00f017de4a3f
machunpo <34728528+machunpo@users.noreply.github.com>
2020-08-03 20:52:28 +0800

    Update test.ino

--- clock/test/test.ino
+++ clock/test/test.ino
@@ -9,13 +9,13 @@ int8_t Year = 20;
 int8_t Moon = 8;
 int8_t Day = 2;
 int8_t Hour = 20;
-int8_t Min = 30;
+int8_t Min = 47;
 int8_t Sec = 0;
 
 void setup()
 {
   tm1637.init();
-  tm1637.set(BRIGHT_DARKEST); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  tm1637.set(BRIGHT_DARKEST); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7
   tm1637.point(POINT_ON);
   tm1637.display(TimeDisp);
   delay(2500);
@@ -26,51 +26,25 @@ void setup()
 
 void loop()
 {
+  
+  Sec++;
+  if(Sec == 60) {
+       Sec = 0;
+       Min++;
+       if(Min == 60){
+           Min = 0;
+           Hour++;
+           if(Hour == 24){
+               Hour = 0;}}}
 
-  for (; Hour < 24; Hour++)
-  {
-    for (; Min < 60; Min++)
-    {
-      for (; Sec < 60; Sec++)
-      {
         TimeDisp[0] = Hour / 10;
         TimeDisp[1] = Hour % 10;
         TimeDisp[2] = Min / 10;
         TimeDisp[3] = Min % 10;
         tm1637.point(POINT_ON);
         tm1637.display(TimeDisp);
-        delay(500);
+        delay(499);
         tm1637.point(POINT_OFF);
         tm1637.display(TimeDisp);
-        delay(500);
-      }
-      Sec = 0;
-    }
-    Min = 0;
-  }
-  Hour = 0;
+        delay(499);
 }
-// Whether to light the clock point ":".是否点亮时钟点“：
-// To take effect the next time it displays.下次显示时生效
-/*
-
-if(count == 20)
-         {
-            count = 0;
-            sec++;
-            if(sec == 60)
-            {
-                sec = 0;
-                min++;
-                if(min == 60)
-                {
-                    min = 0;
-                    hour++;
-                    if(hour == 24)
-                    {
-                        hour = 0;
-                    }
-                }
-            }
-
-*/
